e6597a80711f893103be719f0c4205cbb689549f
Kyle Kastner <kkastner@kkastner-MacBookAir.(none)>
2013-02-08 00:21:29 -0600

    Added first pass for reading IR data

--- dsp/spectral_kurtosis.py
+++ dsp/spectral_kurtosis.py
@@ -36,6 +36,11 @@ except SystemExit:
 if args.filename[-4:] == ".wav":
     sr, data = wavfile.read(args.filename)
     data = np.asarray(data, dtype=np.complex64)[::args.endpoints[2]]
+elif args.filename[-4:] == ".asc":
+    all_sensor_data = pd.read_csv(args.filename, sep="\t", skiprows=2)
+    data = all_sensor_data['Mic[Pa]']
+    #data = all_sensor_data['accel_Y[g]']
+    #data = all_sensor_data['P_waterjacket[bar]']
 
 def overlap_data_stream(data, chunk=256, overlap_percentage=.75):
     chunk_count = len(data)/chunk
@@ -46,7 +51,7 @@ def overlap_data_stream(data, chunk=256, overlap_percentage=.75):
     strides = (data.itemsize*(chunk-overlap_samples), data.itemsize)
     return ast(data, shape=shape, strides=strides)
 
-FFT_SIZE=128
+FFT_SIZE=256
 TRANSIENT_TIME_S=.075
 SAMPLES_PER_S=44100
 overlapped = overlap_data_stream(data, chunk=FFT_SIZE, overlap_percentage=0).T
@@ -55,21 +60,25 @@ raw_spectrogram = np.fft.fftshift(np.fft.fft(windowed_overlapped, n=FFT_SIZE, ax
 log_abs_spectrogram = np.log(np.abs(raw_spectrogram))
 window_length = int(float((TRANSIENT_TIME_S)*SAMPLES_PER_S)/FFT_SIZE)
 if window_length < 3:
-    print "Unable to process, increse TRANSIENT_TIME_S to a larger value!"
-    sys.exit()
+    print "Unable to process, increase TRANSIENT_TIME_S to a larger value!"
+    print "Setting to minimal transient time"
+    window_length = 25
 spec_dframe = pd.DataFrame(np.abs(raw_spectrogram))
 #spec_dframe[0] is the same as np.abs(raw_spectrogram[:,0]), which means each row represents an FFT for a certain period of time
 rolling_kurtosis = pd.rolling_kurt(spec_dframe, window_length, axis=1).fillna()
-f, axarr = plot.subplots(2,sharex=True)
+f, axarr = plot.subplots(3)
 #colors.normalize takes in vmin and vmax to try to set the colormap - this should do the same as
 #the default argument to norm but I wanted to document this for changing later
-axarr[0].imshow(log_abs_spectrogram,
+axarr[0].set_xlim([0,FFT_SIZE*rolling_kurtosis.shape[1]])
+axarr[0].set_autoscalex_on(False)
+axarr[0].plot(data)
+axarr[1].imshow(log_abs_spectrogram,
         norm=colors.normalize(vmin=np.amin(log_abs_spectrogram),
                 vmax=np.amax(log_abs_spectrogram),
                 clip=False),
         cmap=cm.jet,
         aspect='normal')
-axarr[1].imshow(rolling_kurtosis,
+axarr[2].imshow(rolling_kurtosis,
         norm=colors.normalize(vmin=rolling_kurtosis.min().min(),
             vmax=rolling_kurtosis.max().max(),
             clip=False),
