e7aa5f02d11215c996dd71e20860160fc164f9c7
Kyle Kastner <kkastner@kkastner-MacBookAir.(none)>
2013-05-04 00:19:04 -0500

    Overflow errors in optimized code

--- dsp/matrix_factorization.py
+++ dsp/matrix_factorization.py
@@ -17,78 +17,63 @@ from scipy.misc import lena
 def lowrank_SVD(input_matrix, approx=50):
     U,S,VT = np.linalg.svd(A, full_matrices=False)
     A_ = np.zeros((len(U), len(VT)))
-    e = 0
     for i in xrange(K):
         A_ += S[i]*np.outer(U.T[i],VT[i])
-        e += (A[i,:]-A_[i,:])**2
-    RMSE = e
-    return A_, RMSE
+    return A_
 
-def PMF(input_matrix, approx=50, iterations=30, learning_rate=.001, regularization_rate=.1, randomize=True, print_status=True):
+def PMF(input_matrix, approx=50, iterations=30, learning_rate=.001, regularization_rate=.1):
     A = input_matrix
     K = approx
-    A = input_matrix
-    K = approx
-    itr = iterations
-    a = learning_rate
+    R = itr = iterations
+    l = learning_rate
     b = regularization_rate
     N = A.shape[0]
     M = A.shape[1]
     U = np.random.randn(N,K)
     V = np.random.randn(K,M)
-    if randomize:
-        import random
-    RMSE=[]
-    if print_status:
-        print "Starting PMF"
-    I = A > 0
-    for r in range(itr):
-        e = 0
-        for i in range(N):
-            for j in range(M):
-                if I[i,j]:
-                    eij = A[i,j] - np.dot(U[i,:],V[:,j])
-                    e += eij
-                    U[i,:] = U[i,:] + a*(eij*V[:,j] - b*U[i,:])
-                    V[:,j] = V[:,j] + a*(eij*U[i,:] - b*V[:,j])
-        RMSE.append(e)
-        if print_status:
-            print "Iteration " + `r` + " RMSE: " + `RMSE[-1]**2`
-    A_ = np.dot(U,V)
-    return A_,RMSE
+    Z = np.asarray(A > 0,dtype=np.int)
+    opt = False
+    opt = True
+    from scipy.weave import blitz
+    if not opt:
+        for r in range(itr):
+            for i in range(N):
+                for j in range(M):
+                    if Z[i,j]:
+                        e = A[i,j] - np.dot(U[i,:],V[:,j])
+                        U[i,:] = U[i,:] + l*(e*V[:,j] - b*U[i,:])
+                        V[:,j] = V[:,j] + l*(e*U[i,:] - b*V[:,j])
+    else:
+    #http://technicaldiscovery.blogspot.com/2011/06/speeding-up-python-numpy-cython-and.html
+        from scipy.weave import inline
+        from scipy.weave import converters
+        weave_options = {'extra_compile_args': ['-O3'],
+                         'compiler': 'gcc'}
+        code = \
+r"""
+int r,i,j,k;
+double e;
+for(r=0; r<R; r++){
+    for(i=0; i<N; i++){
+        for(j=0; j<M; j++){
+            for(k=0; k<K; k++){
+                if(Z(i,j)){
+                    e = A(i,j)-(U(i,k)*V(k,j));
+                    U(i,k) += l*(e*V(k,j)-b*U(i,k));
+                    V(k,j) += l*(e*U(i,k)-b*V(k,j));
+                }
+            }
+        }
+    }
+}
+"""
+        inline(code,
+               ['A','K','R','l','b','N','M','U','V','Z'],
+               type_converters=converters.blitz,
+               **weave_options)
 
-def KPMF(input_matrix, approx=50, iterations=30, learning_rate=.001, regularization_rate=.1, randomize=True, print_status=True):
-    A = input_matrix
-    K = approx
-    A = input_matrix
-    K = approx
-    itr = iterations
-    a = learning_rate
-    b = regularization_rate
-    N = A.shape[0]
-    M = A.shape[1]
-    U = np.random.randn(N,K)
-    V = np.random.randn(K,M)
-    if randomize:
-        import random
-    RMSE=[]
-    if print_status:
-        print "Starting PMF"
-    I = A > 0
-    for r in range(itr):
-        e = 0
-        for i in range(N):
-            for j in range(M):
-                if I[i,j]:
-                    eij = A[i,j] - np.dot(U[i,:],V[:,j])
-                    e += eij
-                    U[i,:] = U[i,:] + a*(eij*V[:,j] - b*U[i,:])
-                    V[:,j] = V[:,j] + a*(eij*U[i,:] - b*V[:,j])
-        RMSE.append(e)
-        if print_status:
-            print "Iteration " + `r` + " RMSE: " + `RMSE[-1]**2`
     A_ = np.dot(U,V)
-    return A_,RMSE
+    return A_
 
 #Rework of lena example
 #From https://gist.github.com/thearn/5424219
@@ -96,9 +81,9 @@ def KPMF(input_matrix, approx=50, iterations=30, learning_rate=.001, regularizat
 approx = K = 50
 iterations = I = 10
 A = lena()
-A_,RMSE=lowrank_SVD(A,approx=K)
+A_=lowrank_SVD(A,approx=K)
 plot.figure()
-plot.title("Low Rank SVD (full matrix) RMSE = " + `RMSE[-1]`)
+plot.title("Low Rank SVD (full matrix) RMSE = ")
 plot.imshow(A_, cmap=cm.gray)
 
 #Sparse matrix setup
@@ -115,21 +100,21 @@ plot.title("Sparse Lena")
 plot.imshow(A, cmap=cm.gray)
 
 #Sparse matrix, regular SVD example, low rank approximation
-A_,RMSE=lowrank_SVD(A,approx=K)
+A_=lowrank_SVD(A,approx=K)
 plot.figure()
-plot.title("Low Rank SVD, RMSE = " + `RMSE[-1]`)
+plot.title("Low Rank SVD, RMSE = ")
 plot.imshow(A_, cmap=cm.gray)
 
 #Sparse matrix, gradient descent example
-A_,RMSE=PMF(A,approx=K,iterations=I)
+A_=PMF(A,approx=K,iterations=I)
 plot.figure()
-plot.title("PMF, RMSE = " + `RMSE[-1]`)
+plot.title("PMF, RMSE = ")
 plot.imshow(A_, cmap=cm.gray)
 plot.show()
 
 #Sparse matrix, constrained gradient descent example
-A_,RMSE=KPMF(A,approx=K,iterations=I)
-plot.figure()
-plot.title("Kernelized PMF, RMSE = " + `RMSE[-1]`)
-plot.imshow(A_, cmap=cm.gray)
-plot.show()
+#A_,RMSE=KPMF(A,approx=K,iterations=I)
+#plot.figure()
+#plot.title("Kernelized PMF, RMSE = " + `RMSE[-1]`)
+#plot.imshow(A_, cmap=cm.gray)
+#plot.show()
