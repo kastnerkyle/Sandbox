d96749f7224eb9195e10cb91e70ebd99cb7032fe
Kyle Kastner <kkastner@kkastner-MacBookAir.(none)>
2013-05-05 01:56:00 -0500

    Adding most of KPMF, will finish tomoorw (probably need to stick with gradient descent first pass

--- dsp/matrix_factorization.py
+++ dsp/matrix_factorization.py
@@ -3,6 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plot
 import matplotlib.cm as cm
 from scipy.misc import lena
+#To get the latest scipy (for banded matrix generation
+#sudo apt-get install python python-dev gfortran libatlas-base-dev
+#sudo pip install scipy
+import scipy.sparse as sp
+import scipy.linalg as sl
 #Example from
 #Singular Value Decomposition Tutorial
 #Kirk Baker
@@ -45,6 +50,51 @@ def PMF(input_matrix, approx=50, iterations=30, learning_rate=.001, regularizati
     A_ = np.dot(U,V)
     return A_+mean
 
+def KPMF(input_matrix, approx=50, iterations=30, learning_rate=.001, regularization_rate=.1):
+    A = input_matrix
+    Z = np.asarray(A > 0,dtype=np.int)
+    A1d = np.ravel(A)
+    mean = np.mean(A1d)
+    A = A-mean
+    K = approx
+    R = itr = iterations
+    l = learning_rate
+    b = regularization_rate
+    N = A.shape[0]
+    M = A.shape[1]
+    U = np.random.randn(N,K)
+    V = np.random.randn(K,M)
+    #Using diffusion kernel with beta=.5
+    #U is the rows, we use an adjacency matrix C
+    #This matrix assumes that rows +- 5 are connected to each other
+    #Columns +- 5 are connected as well
+    #This forms a spatial smoothness graph
+    #See Kernelized Probabilistic Matrix Factorization: Exploiting Graphs and Side Information
+    #T. Zhou H. Shan A. Banerjee G. Sapiro
+    bw = bandwidth = 10
+    #Use scipy.sparse.diags to generate band matrix
+    CU = sp.diags([1]*bw,range(-bw/2,bw/2),shape=(N,N)).todense()
+    DU = np.diagflat(np.sum(CU,1))
+    CV = sp.diags([1]*bw,range(-bw/2,bw/2),shape=(M,M)).todense()
+    DV = np.diagflat(np.sum(CV,1))
+    LU = DU - CU
+    LV = DV - CV
+    beta = .5
+    KU = sl.expm(-beta*LU)
+    KV = sl.expm(-beta*LV)
+    SU = np.linalg.pinv(KU)
+    SV = np.linalg.pinv(KV)
+    for r in range(R):
+        for i in range(N):
+            for j in range(M):
+                if Z[i,j] > 0:
+                    e = A[i,j] - np.dot(U[i,:],V[:,j])
+                    #Magic here?
+                    U[i,:] = U[i,:] + l*(e*V[:,j])
+                    V[:,j] = V[:,j] + l*(e*U[i,:])
+    A_ = np.dot(U,V)
+    return A_+mean
+
 #Rework of lena example
 #From https://gist.github.com/thearn/5424219
 #Full matrix SVD, low rank approximation
@@ -76,15 +126,14 @@ plot.title("Low Rank SVD, RMSE = ")
 plot.imshow(A_, cmap=cm.gray)
 
 #Sparse matrix, gradient descent example
-A_=PMF(A,approx=K,iterations=I)
-plot.figure()
-plot.title("PMF, RMSE = ")
-plot.imshow(A_, cmap=cm.gray)
-plot.show()
+#A_=PMF(A,approx=K,iterations=I)
+#plot.figure()
+#plot.title("PMF, RMSE = ")
+#plot.imshow(A_, cmap=cm.gray)
 
 #Sparse matrix, constrained gradient descent example
-#A_,RMSE=KPMF(A,approx=K,iterations=I)
-#plot.figure()
-#plot.title("Kernelized PMF, RMSE = " + `RMSE[-1]`)
-#plot.imshow(A_, cmap=cm.gray)
+A_=KPMF(A,approx=K,iterations=I)
+plot.figure()
+plot.title("Kernelized PMF, RMSE = ")
+plot.imshow(A_, cmap=cm.gray)
 #plot.show()
